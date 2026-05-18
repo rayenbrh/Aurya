2a84c6e3ad2bc07c0a57ade9cbc6f267eff90e2e
Rayen Brahem <brahemrayen03@gmail.com>
2026-05-18 13:31:34 +0100

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['gouidex-front.2bj94x.easypanel.host'],
+    allowedHosts: ['https://auryadeco.tn/'],
   },
   plugins: [
     react(),
